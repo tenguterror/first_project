211652dc9b2ff8377ef2cd42794cd2b09679f65f
TenguTerror <vegasduard@gmail.com>
2023-08-14 20:13:39 -0600

    Added last data for concepts

--- src/Components/Concepts.js
+++ src/Components/Concepts.js
@@ -2,13 +2,29 @@ import React from 'react';
 
 const Concepts = (props) => {
   return (
-    <ul id='concepts'>
-      <li className='concept'>
-        <img src={props.items[0].image} alt='Stairs of components' />
-        <h2>{props.items[0].title}</h2>
-        <p>{props.items[0].description}</p>
-      </li>
-    </ul>
+    <div>
+      <ul id='concepts'>
+        <li className='concept'>
+          <img src={props.items[0].image} alt='Stairs of components' />
+          <h2>{props.items[0].title}</h2>
+          <p>{props.items[0].description}</p>
+        </li>
+      </ul>
+      <ul id='concepts'>
+        <li className='concept'>
+          <img src={props.items[1].image} alt='Data structure of state' />
+          <h2>{props.items[1].title}</h2>
+          <p>{props.items[1].description}</p>
+        </li>
+      </ul>
+      <ul id='concepts'>
+        <li className='concept'>
+          <img src={props.items[2].image} alt='Micropone of events' />
+          <h2>{props.items[2].title}</h2>
+          <p>{props.items[2].description}</p>
+        </li>
+      </ul>
+    </div>
   );
 };
 
