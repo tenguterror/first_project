e15cec278de59a666d96e685c224fa0f1c28c098
TenguTerror <vegasduard@gmail.com>
2023-08-14 20:06:09 -0600

    Basic formatting

--- src/Components/Concepts.js
+++ src/Components/Concepts.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Concepts = (props) => {
   return (
-    <ul id="concepts">
-        <li className="concept">
-          <img src={props.items[0].image} alt="Stairs of components" />
-          <h2>{props.items[0].title}</h2>
-          <p>{props.items[0].description}</p>
-        </li>
-      </ul>
-  )
+    <ul id='concepts'>
+      <li className='concept'>
+        <img src={props.items[0].image} alt='Stairs of components' />
+        <h2>{props.items[0].title}</h2>
+        <p>{props.items[0].description}</p>
+      </li>
+    </ul>
+  );
 };
-  
+
 export default Concepts;
