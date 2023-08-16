b5f92cf62c8705959a071943bf34dda379d8c617
TenguTerror <vegasduard@gmail.com>
2023-08-15 18:51:26 -0600

    redid the concepts ul to make items side by side

--- src/Components/Concepts.js
+++ src/Components/Concepts.js
@@ -10,15 +10,11 @@ const Concepts = (props) => {
           <h2>{props.items[0].title}</h2>
           <Description description={props.items[0].description} />
         </li>
-      </ul>
-      <ul id='concepts'>
         <li className='concept'>
           <img src={props.items[1].image} alt={props.items[1].title} />
           <h2>{props.items[1].title}</h2>
           <Description description={props.items[1].description} />
         </li>
-      </ul>
-      <ul id='concepts'>
         <li className='concept'>
           <img src={props.items[2].image} alt={props.items[2].title} />
           <h2>{props.items[2].title}</h2>
